595dbc33a1376c6b71185d241343d1a9b96ea485
yamada-kaito <d0_0b-4y2mks0904@yahoo.ne.jp>
2025-04-06 21:12:26 +0900

    ✨ ユーザー登録機能作成

--- laravel/app/Http/Controllers/Auth/RegisteredUserController.php
+++ laravel/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,8 +30,8 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'name' => ['required', 'string', 'alpha_num', 'min:3', 'max:16', 'unique:users'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -45,6 +45,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('index', absolute: false));
     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ArticleController::class, 'index']);
+Route::get('/', [ArticleController::class, 'index'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
