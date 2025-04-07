9ad4c0d3cd15ea0e8a92d93df01655413a31f181
yamada-kaito <d0_0b-4y2mks0904@yahoo.ne.jp>
2025-04-07 18:13:27 +0900

    🔧 ⬆️ viteを使用してvueコンポーネントが表示されるように修正

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+    },
+  },
+  plugins: [
+    vue(),
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  server: {
+    host: true,
+    hmr: {
+      host: 'localhost',
+    },
+  },
 });
