59be4395410c3adeef42088cd97c6f67cc50fbb6
yamada-kaito <d0_0b-4y2mks0904@yahoo.ne.jp>
2025-04-07 13:37:56 +0900

    ✨ 記事更新・削除・詳細表示機能作成

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,8 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 Route::middleware('auth')->group(function () {
-    Route::resource('/articles', ArticleController::class)->except(['index']);
+    Route::resource('/articles', ArticleController::class)->except(['index', 'show']);
 });
+Route::resource('/articles', ArticleController::class)->only(['show']);
+
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
