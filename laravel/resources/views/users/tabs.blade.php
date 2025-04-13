75f07632ea3376f6c2af20b19b943f213d1a9f4b
yamada-kaito <d0_0b-4y2mks0904@yahoo.ne.jp>
2025-04-13 21:22:04 +0900

    🎨 タブstyle修正

--- laravel/resources/views/users/tabs.blade.php
+++ laravel/resources/views/users/tabs.blade.php
@@ -1,13 +1,11 @@
 <ul class="nav nav-tabs nav-justified mt-3" style="border-bottom: 1px solid #e1e8ed;">
   <li class="nav-item">
-    <a class="nav-link {{ $hasArticles ? 'active' : '' }}" href="{{ route('users.show', ['name' => $user->name]) }}"
-      style="color: #1da1f2; font-weight: 600;">
+    <a class="nav-link {{ $hasArticles ? 'active' : '' }}" href="{{ route('users.show', ['name' => $user->name]) }}">
       記事
     </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ $hasLikes ? 'active' : '' }}" href="{{ route('users.likes', ['name' => $user->name]) }}"
-      style="color: #1da1f2; font-weight: 600;">
+    <a class="nav-link {{ $hasLikes ? 'active' : '' }}" href="{{ route('users.likes', ['name' => $user->name]) }}">
       いいね
     </a>
   </li>
